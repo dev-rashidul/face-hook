381e356cd21e58ad389d6fb1ca37c108ec8b5b20
Rashidul Islam <rashidul.contact@gmail.com>
2024-05-05 15:25:05 +0600

    Initial Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import ProfilePage from "./pages/ProfilePage"
+import RegistrationPage from "./pages/RegistrationPage"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route element={<HomePage/>} path="/" exact/>
+        <Route element={<LoginPage/>} path="/login"/>
+        <Route element={<RegistrationPage/>} path="/registration"/>
+        <Route element={<ProfilePage/>} path="/profile"/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div>
+            <h2>This is LoginPage</h2>
+        </div>
+    );
+};
+
+export default LoginPage;
